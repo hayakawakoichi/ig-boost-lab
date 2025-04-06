dc99d83fd77fe3210e2c0b3d343f85287938d1c2
hayakawakoichi <koichi.hayakawa.dev@gmail.com>
2025-04-06 21:51:23 +0900

    test

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from '@/components/ui/card'
+import { Card } from '@/components/ui/card'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
 import { Input } from '@/components/ui/input'
